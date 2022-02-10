edd7891ebaa4587214652faac16bec87a73d65b9
maxvl3 <maxvanliempdt@gmail.com>
2022-02-10 22:22:36 +0100

    huiswerk

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("hallo test");
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hoi!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
