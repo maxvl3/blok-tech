a5c5293ac11d4a8d2af82515b645ab0dc305b503
maxvl3 <maxvanliempdt@gmail.com>
2022-03-04 10:47:59 +0100

    formulier gemaakt

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/*
 const express = require('express')
 const app = express()
 const { engine } = require('express-handlebars')
@@ -23,3 +24,34 @@ function naarLogIn(req, res) {
 app.listen(PORT, () => {
     console.log('app running on port', PORT);
 })
+*/
+
+var express = require('express');
+var bodyParser = require('body-parser');
+var multer = require('multer');
+var upload = multer();
+var app = express();
+
+app.get('/', function(req, res){
+   res.render('form');
+});
+
+app.set('view engine', 'pug');
+app.set('views', './view');
+
+// for parsing application/json
+app.use(bodyParser.json()); 
+
+// for parsing application/xwww-
+app.use(bodyParser.urlencoded({ extended: true })); 
+//form-urlencoded
+
+// for parsing multipart/form-data
+app.use(upload.array());
+app.use('/static', express.static('static'))
+
+app.post('/', function(req, res){
+   console.log(req.body);
+   res.send("recieved your request!");
+});
+app.listen(8000);
