dbb12c07c5c9c8806e7166087337923b1e7220ca
maxvl3 <maxvanliempdt@gmail.com>
2022-04-13 22:56:01 +0200

    Geolocation API

--- static/javascript/script.js
+++ static/javascript/script.js
@@ -20,14 +20,6 @@ document
     }
   });
 
-  function getLocation() {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(showPosition);
-    } else {
-      console.log("Geolocation is not supported by this browser.");
-    }
-  }
-
   navigator.geolocation.getCurrentPosition((pos) => {
     myFunction(pos.coords.latitude, pos.coords.longitude);
   });
